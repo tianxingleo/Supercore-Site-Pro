92f26f8724769c1a1d91423f2051c66cc919500c
tianxingleo <tianxingleo@gmail.com>
2026-01-26 11:24:37 +0800

    Reset project by removing all source and configuration files, and regenerating build artifacts and node modules.

--- layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="min-h-screen">
-    <slot />
-  </div>
-</template>
-
-<script setup lang="ts">
-// Default layout - can be extended later for specific page layouts
-</script>
